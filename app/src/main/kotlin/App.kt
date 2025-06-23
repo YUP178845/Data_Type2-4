61076ce8d8752fba06c80dc082dcbea7f1c3237a
soponvitk keawkong <soponvitk2551@gmail.com>
2025-06-23 12:33:38 +0700

    first commit

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,27 @@
+fun main(){
+    kop()
+}
+
+
+fun kop(){
+    var Kint: Int = 20_000
+    var kboo: Boolean = true
+    var KStr: String = "noob"
+    var kdo: Double = 845.00
+    var KA: Any = "fuck you"
+
+    println("$Kint \n $kboo \n $KStr \n $KA \n $kdo")
+    println("-------------------------------------------")
+    var a: Double = 2.0
+    var b: Double = 4.0
+    println(a%b)
+
+
+
+
+
+
+
+
+
+}
